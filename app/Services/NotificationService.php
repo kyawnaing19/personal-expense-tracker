49a9c3665587f25403f0ed730f860815c30c22f7
kyawnaing19 <kyawkyawnaing2812001@gmail.com>
2026-07-03 15:40:09 +0630

    add log into noti send to user try catch

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -20,11 +20,17 @@ class NotificationService
         }
 
         try {
+          \Log::info('FCM Step 1: Preparing cloud message', ['user_id' => $user->id]);
+
             $message = CloudMessage::withTarget('token', $user->fcm_token)
                 ->withNotification(Notification::create($title, $body))
                 ->withData($data);
 
+            \Log::info('FCM Step 2: Message created successfully, sending to Firebase...', ['user_id' => $user->id]);
+
             $this->messaging->send($message);
+
+            \Log::info('FCM Step 3: Notification sent successfully!', ['user_id' => $user->id]);
         } catch (\Throwable $e) {
 
             \Log::warning('FCM notification failed', [
