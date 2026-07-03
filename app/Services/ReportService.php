258f4b5c49f0726caef27c7f1667e78c34f7e581
kyawnaing19 <kyawkyawnaing2812001@gmail.com>
2026-07-03 10:09:50 +0630

    -catefory breakdown report update getdata range

--- app/Services/ReportService.php
+++ app/Services/ReportService.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Services;
 
 use App\Repositories\ReportRepository;
@@ -9,51 +10,54 @@ class ReportService
     public function __construct(
         private ReportRepository $reportRepository
 
-    ){}
+    ) {}
 
-    public function getDataRange(array $filters=[]):array
+    public function getDataRange(array $filters = []): array
     {
-        $filter=$filters['filter'] ??'this_month';
-        return match($filter){
-            'today'=>[
-                'from'=>Carbon::today()->toDateString(),
-                'to'=>Carbon::today()->toDateString()],
-            'yesterday'=>[
-                'from'=>Carbon::yesterday()->toDateString(),
-                'to'=>Carbon::yesterday()->toDateString()],
-            'this_month'=>[
-                'from'=>Carbon::now()->startOfMonth()->toDateString(),
-                'to'=>Carbon::now()->endOfMonth()->toDateString()],
-            'last_month'=>[
-                'from'=>Carbon::now()->subMonth()->startOfMonth()->toDateString(),
-                'to'=>Carbon::now()->subMonth()->endOfMonth()->toDateString()],
-            'custom'=>[
-                'from'=>$filters['from'],
-                'to'=>$filters['to']],
-            default=>[
-                'from'=>Carbon::now()->startOfMonth()->toDateString(),
-                'to'=>Carbon::now()->endOfMonth()->toDateString()
+        $filter = $filters['filter'] ?? 'this_month';
+
+        return match ($filter) {
+            'today' => [
+                'from' => Carbon::today()->toDateString(),
+                'to' => Carbon::today()->toDateString()],
+            'yesterday' => [
+                'from' => Carbon::yesterday()->toDateString(),
+                'to' => Carbon::yesterday()->toDateString()],
+            'this_month' => [
+                'from' => Carbon::now()->startOfMonth()->toDateString(),
+                'to' => Carbon::now()->endOfMonth()->toDateString()],
+            'last_month' => [
+                'from' => Carbon::now()->subMonth()->startOfMonth()->toDateString(),
+                'to' => Carbon::now()->subMonth()->endOfMonth()->toDateString()],
+            'custom' => [
+                'from' => $filters['from'],
+                'to' => $filters['to']],
+            default => [
+                'from' => Carbon::now()->startOfMonth()->toDateString(),
+                'to' => Carbon::now()->endOfMonth()->toDateString(),
             ]
         };
     }
 
-
-    public function getSummary(string $userId,array $filter=[])
+    public function getSummary(string $userId, array $filter = [])
     {
-        $dataRange=$this->getDataRange($filter);
-        return $this->reportRepository->getSummary($userId,$dataRange);
+        $dataRange = $this->getDataRange($filter);
+
+        return $this->reportRepository->getSummary($userId, $dataRange);
     }
 
-    public function getCategoryBreakdown(string $userId, array $filter=[])
+    public function getCategoryBreakdown(string $userId, array $filter = [])
     {
-        $dataRange=$this->getDataRange($filter);
-        return $this->reportRepository->getCategoryBreakdown($userId,$dataRange);
+        $dataRange = $this->getDataRange($filter);
+
+        return $this->reportRepository->getCategoryBreakdown($userId, $dataRange);
     }
 
-    public function getBudgetOverview(string $userId,array $filter=[])
+    public function getBudgetOverview(string $userId, array $filter = [])
     {
-        $month=$filter['month'];
-        $year=$filter['year'];
-        return $this->reportRepository->getBudgetOverview($userId,$month,$year);
+        $month = $filter['month'];
+        $year = $filter['year'];
+
+        return $this->reportRepository->getBudgetOverview($userId, $month, $year);
     }
 }
